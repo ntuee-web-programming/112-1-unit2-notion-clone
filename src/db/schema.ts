5ca7c935cec989b4f7fcd4d0b2348bb1f641d878
ba144220 <hs41511@gmail.com>
2023-11-07 13:13:51 +0800

    feature: complete usersTable schema and User type

---
+++ src/db/schema.ts
@@ -0,0 +1,22 @@
+import { index, pgTable, serial, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable(
+  "users",
+  {
+    id: serial("id").primaryKey(),
+    displayId: uuid("display_id").defaultRandom().notNull().unique(),
+    username: varchar("username", { length: 100 }).notNull(),
+    email: varchar("email", { length: 100 }).notNull().unique(),
+    hashedPassword: varchar("hashed_password", { length: 100 }),
+    provider: varchar("provider", {
+      length: 100,
+      enum: ["github", "credentials"],
+    })
+      .notNull()
+      .default("credentials"),
+  },
+  (table) => ({
+    displayIdIndex: index("display_id_index").on(table.displayId),
+    emailIndex: index("email_index").on(table.email),
+  }),
+);

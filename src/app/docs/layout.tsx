2e215140ef544fd83790f280a645dd6dacc6f1b0
ba144220 <hs41511@gmail.com>
2023-11-07 14:26:04 +0800

    feature: separate scrollable layout and parallel routes

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -5,10 +5,13 @@ type Props = {
 
 function DocsLayout({ children, navbar }: Props) {
   return (
-    <main className="flex-rows fixed top-0 flex h-screen w-full overflow-hidden ">
+    // overflow-hidden for parent to hide scrollbar
+    <main className="flex-rows fixed top-0 flex h-screen w-full overflow-hidden">
+      {/* overflow-y-scroll for child to show scrollbar */}
       <nav className="flex w-2/5 flex-col overflow-y-scroll border-r bg-slate-100 pb-10">
         {navbar}
       </nav>
+      {/* overflow-y-scroll for child to show scrollbar */}
       <div className="w-full overflow-y-scroll">{children}</div>
     </main>
   );

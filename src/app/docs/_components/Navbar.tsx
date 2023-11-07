b36a73c90f8ff96878ff7397feb25d9fbac57b35
ba144220 <hs41511@gmail.com>
2023-11-07 16:27:24 +0800

    feature: create document

--- src/app/docs/_components/Navbar.tsx
+++ src/app/docs/_components/Navbar.tsx
@@ -8,11 +8,14 @@ import { Button } from "@/components/ui/button";
 import { auth } from "@/lib/auth";
 import { publicEnv } from "@/lib/env/public";
 
+import { createDocument } from "./actions";
+
 async function Navbar() {
   const session = await auth();
   if (!session || !session?.user?.id) {
     redirect(publicEnv.NEXT_PUBLIC_BASE_URL);
   }
+  const userId = session.user.id;
   return (
     <nav className="flex w-full flex-col overflow-y-scroll bg-slate-100 pb-10">
       <nav className="sticky top-0 flex flex-col items-center justify-between border-b bg-slate-100 pb-2">
@@ -34,7 +37,13 @@ async function Navbar() {
           </Link>
         </div>
 
-        <form className="w-full hover:bg-slate-200">
+        <form
+          className="w-full hover:bg-slate-200"
+          action={async () => {
+            "use server";
+            await createDocument(userId);
+          }}
+        >
           <button
             type="submit"
             className="flex w-full items-center gap-2 px-3 py-1 text-left text-sm text-slate-500"
---
+++ src/app/docs/_components/actions.ts
@@ -0,0 +1,23 @@
+import { db } from "@/db";
+import { documentsTable, usersToDocumentsTable } from "@/db/schema";
+
+export const createDocument = async (userId: string) => {
+  "use server";
+  console.log("[createDocument]");
+
+  const newDocId = await db.transaction(async (tx) => {
+    const [newDoc] = await tx
+      .insert(documentsTable)
+      .values({
+        title: "New Document",
+        content: "This is a new document",
+      })
+      .returning();
+    await tx.insert(usersToDocumentsTable).values({
+      userId: userId,
+      documentId: newDoc.displayId,
+    });
+    return newDoc.displayId;
+  });
+  return newDocId;
+};
